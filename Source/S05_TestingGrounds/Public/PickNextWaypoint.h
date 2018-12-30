5a0fca8524cc20b2f8a85740506e04abcaba4a93
Johan Hansson <johan.hansson.carlberg@gmail.com>
2018-12-30 16:08:26 +0100

    Created class for porting PickNextWaypoint behaviour tree task to C++

---
+++ Source/S05_TestingGrounds/Public/PickNextWaypoint.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/BTTaskNode.h"
+#include "PickNextWaypoint.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class S05_TESTINGGROUNDS_API UPickNextWaypoint : public UBTTaskNode
+{
+	GENERATED_BODY()
+	
+};
